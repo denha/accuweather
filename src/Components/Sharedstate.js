93a928de712f7d81005ddccd9f44d72ee2c42813
Denis Ssamba <denis.ssamba@tuni.fi>
2022-02-17 21:50:24 +0200

    FIX DEFAULT CITY

--- src/Components/Sharedstate.js
+++ src/Components/Sharedstate.js
@@ -4,7 +4,7 @@ import {useEffect,useState} from 'react'
 const Sharedstate = () =>{
 const key = "7AGZWdPRVGBVwhlorKHGovCbWy7cmBmN";
 const baseUrl = "https://dataservice.accuweather.com/locations/v1/cities/search";
-const[cityDefault,setCityDefault] = useState('Tampere');
+const[cityDefault,setCityDefault] = useState('');
 const query = `?apikey=${key}&q=${cityDefault}`;
 const baseUrlCity = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/";
 const [forecast,setForecast] = useState({});
